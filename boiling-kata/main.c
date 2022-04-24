f85c72db0c089d8a238211f0bf965f6abc153f24
Marcin <31159047+lazyspot@users.noreply.github.com>
2022-04-24 16:43:26 +0200

    Add new version of bowling kata algorythm

--- boiling-kata/main.c
+++ boiling-kata/main.c
@@ -6,18 +6,39 @@ char score_array[21];
 int score() {
     int result =0;
     for(int i = 0; i < 21; ++i) {
-        result += score_array[i];
+        if(score_array[i] == -1) break;
+        if(score_array[i] == 10) {
+            result += 20;
+            continue;
+        }
+        if(score_array[i + 1] >= 0) {
+            if((score_array[i] + score_array[i+1]) == 10) {
+                result += 20;
+            }
+            else {
+                result += score_array[i] + score_array[i+1];
+                ++i;
+            }
+        }
     }
     return result;
 };
 
 int main()
 {
-    printf("Welcome in boiling kata\n");
-    printf("Insert values\n");
-    for(int i = 0; i < 21; ++i) {
-        scanf("%i", &score_array[i]);
+    int t;
+    scanf("%d", &t);
+    while(t--) {
+        for(int i = 0; i < 21; ++i) {
+            score_array[i] = -1;
+        }
+        int i = 0;
+        char c;
+        while(scanf("%d%c", &score_array[i], &c)) {
+            if(c != ' ') break;
+            i += 1;
+        }
+        printf("%d\n", score());
     }
-    printf("Result: %i",score());
     return 0;
 }
